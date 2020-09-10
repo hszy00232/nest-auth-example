8cf3d136fc9bedf37dfae4fec7b8c0e589dc7d53
guopeipei <guopeipei@guopeipeideMacBook-Pro-2.local>
2020-09-10 18:56:46 +0800

    项目新增

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { AuthService } from './auth/auth.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+}
